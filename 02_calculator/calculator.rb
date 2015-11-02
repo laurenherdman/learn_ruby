c37bf945694213de91e849d464e806cf70173a34
Lauren <lemherdman@hotmail.com>
2015-11-02 15:23:07 -0500

    all methods complete tests with any number

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,25 +1,19 @@
 def add(first_num, second_num)
-	if first_num == 0 && second_num == 0
-		0
-	elsif first_num == 2 && second_num == 2
-		4
-	else
-		8
-	end
+	first_num + second_num
 end
 
 def subtract(first_sub, second_sub)
-	6
+	if first_sub >= second_sub
+		first_sub - second_sub
+	else
+		second_sub - first_sub
+	end
 end
 
 def sum(sum_array)
 	if sum_array == []
 		0
-	elsif sum_array == [7, 11]
-		18
-	elsif sum_array == [7]
-		7
 	else
-		25
+		sum_array.inject {|sum, n| sum + n}
 	end
 end
